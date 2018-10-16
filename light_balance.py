14db2e319abc9d99b4470cd570717465ee96da57
Kim Ng <kimleeng@gmail.com>
2018-10-16 21:48:46 +0200

    test code

--- light_balance.py
+++ light_balance.py
@@ -1,6 +1,20 @@
 import serial
+import phue
+
 arduinoSerialData = serial.Serial('/dev/ttyACM0',9600)
+
+hue_bridge = Bridge('192.168.1.3')
+hue_bridge.connect()
+
 while True:
     if(arduinoSerialData.inWaiting()>0):
         arduino_data = arduinoSerialData.readline()
         print(arduino_data)
+        analog_value = int(arduino_data.strip())
+        if analog_value < 100:
+            hue_bridge.set_light([5,6], 'bri', 1)
+        elif analog_value > 400:
+            hue_bridge.set_light([5,6], 'bri', 254)
+        else:
+            hue_bridge.set_light([5,6], 'bri', 100)
+
