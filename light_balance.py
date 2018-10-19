bb98d5dff19a2647222d59c47fccd7c5be49b1fd
Kim Ng <kimleeng@gmail.com>
2018-10-19 17:44:56 +0200

    martin

--- light_balance.py
+++ light_balance.py
@@ -16,10 +16,13 @@ while True:
         analog_value = int(arduino_data.strip())
         norm = ((max(min(analog_value, 550), 100) - 100.0)/450)
         print("norm",norm)
-        hue = math.floor(random.random()*65535)
         hue_val = int(math.floor(norm*253) + 1)
         print("hue_val",hue_val)
         hue_bridge.set_light([5,6], 'bri', hue_val, hue=hue)
+        hue = math.floor(random.random()*65535)
+        hue_bridge.set_light([5], 'hue', hue)
+        hue = math.floor(random.random()*65535)
+        hue_bridge.set_light([6], 'hue', hue)
         # if analog_value < 100:
         # elif analog_value > 400:
         #     hue_bridge.set_light([5,6], 'bri', 254)
